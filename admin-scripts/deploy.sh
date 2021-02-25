c3fe86b9dcb76163545c7b933aef2d5d2c11f952
feralcoder <feralcoder@gmail.com>
2021-02-25 10:45:12 +0000

    Split deploy.sh to validate.sh, move inventory file

--- admin-scripts/deploy.sh
+++ admin-scripts/deploy.sh
@@ -2,8 +2,5 @@
 KOLLA_SETUP_SOURCE="${BASH_SOURCE[0]}"
 KOLLA_SETUP_DIR=$( dirname $KOLLA_SETUP_SOURCE )
 
-kolla-genpwd
-ansible -i inventory-feralstack all -m ping
-kolla-ansible -i ./inventory-feralstack bootstrap-servers
-kolla-ansible -i ./inventory-feralstack prechecks
-kolla-ansible -i ./inventory-feralstack deploy
+
+kolla-ansible -i ../files/inventory-feralstack deploy
---
+++ admin-scripts/validate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+KOLLA_SETUP_SOURCE="${BASH_SOURCE[0]}"
+KOLLA_SETUP_DIR=$( dirname $KOLLA_SETUP_SOURCE )
+
+
+kolla-genpwd
+ansible -i ../files/inventory-feralstack all -m ping
+kolla-ansible -i ../files/inventory-feralstack bootstrap-servers
+kolla-ansible -i ../files/inventory-feralstack prechecks
