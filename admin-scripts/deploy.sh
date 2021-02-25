5783dc71c481dc0b1287a46e91d41984820f4571
feralcoder <feralcoder@gmail.com>
2021-02-25 08:59:03 +0000

    All kolla-supplied verification and deploy commands together

--- admin-scripts/deploy.sh
+++ admin-scripts/deploy.sh
@@ -3,6 +3,7 @@ KOLLA_SETUP_SOURCE="${BASH_SOURCE[0]}"
 KOLLA_SETUP_DIR=$( dirname $KOLLA_SETUP_SOURCE )
 
 kolla-genpwd
+ansible -i inventory-feralstack all -m ping
 kolla-ansible -i ./inventory-feralstack bootstrap-servers
 kolla-ansible -i ./inventory-feralstack prechecks
 kolla-ansible -i ./inventory-feralstack deploy
