3042b8d247396dd497fdc181c64bc25efa0047e4
feralcoder <feralcoder@gmail.com>
2021-02-25 22:18:16 +0000

    Check out kolla-ansible before trying to install it...

--- admin-scripts/macro-management/02a_setup_kolla_and_install.sh
+++ admin-scripts/macro-management/02a_setup_kolla_and_install.sh
@@ -7,6 +7,12 @@ ADMIN_SETUP_DIR=$( dirname $ADMIN_SETUP_SOURCE )
 NOW=`date +%Y%m%d-%H%M%S`
 KOLLA_ANSIBLE_CHECKOUT=~/CODE/feralcoder/kolla-ansible/
 LOG_DIR=~/kolla-ansible-logs/
+
+
+ssh_control_wait_for_host_up_these_hosts "$STACK_HOSTS"
+
+ssh_control_run_as_user cliff "cd CODE/feralcoder; git clone https://feralcoder:`cat ~/.git_password`@github.com/feralcoder/kolla-ansible" dmb
+
 ssh_control_run_as_user cliff "mkdir $LOG_DIR" dmb
 ssh_control_run_as_user cliff "$KOLLA_ANSIBLE_CHECKOUT/admin-scripts/setup.sh > $LOG_DIR/01-setup_$NOW.log 2>&1" dmb
 ssh_control_run_as_user cliff "$KOLLA_ANSIBLE_CHECKOUT/admin-scripts/fix-bonds/make_and_setup_stack_bonds.sh > $LOG_DIR/02-bonds_setup_$NOW.log 2>&1" dmb
