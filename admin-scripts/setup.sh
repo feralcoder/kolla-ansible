7c1e9854b00306ae363bdad9fe6c97010df58829
feralcoder <feralcoder@gmail.com>
2021-02-25 07:41:33 +0000

    move add_stack_user_everywhere.sh into setup.sh, fix up setup.sh

--- admin-scripts/setup.sh
+++ admin-scripts/setup.sh
@@ -2,6 +2,9 @@
 KOLLA_SETUP_SOURCE="${BASH_SOURCE[0]}"
 KOLLA_SETUP_DIR=$( dirname $KOLLA_SETUP_SOURCE )
 
+. ~/CODE/feralcoder/host_control/control_scripts.sh
+
+
 
 get_sudo_password () {
   local PASSWORD
@@ -13,6 +16,12 @@ get_sudo_password () {
   echo /tmp/password_$$
 }
 
+setup_local_passwordless_sudo () {
+  cat $SUDO_PASS_FILE | sudo -S ls > /dev/null
+  ( sudo grep "cliff ALL" /etc/sudoers.d/cliff ) || { echo "cliff ALL=(root) NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/cliff; }
+  sudo chmod 0440 /etc/sudoers.d/cliff
+}
+
 new_venv () {
   mkdir -p ~/CODE/venvs/kolla-ansible
   python3 -m venv ~/CODE/venvs/kolla-ansible
@@ -22,19 +31,61 @@ use_venv () {
   source ~/CODE/venvs/kolla-ansible/bin/activate
 }
 
-add_stack_user () {
+
+
+
+add_stack_user_everywhere () {
+  # Add stack user everywhere
+  ssh_control_run_as_user_these_hosts root "adduser stack" "$ALL_HOSTS"
+  echo "Enter stack user password:"
+  PASSFILE=`ssh_control_get_password`
+  mv $PASSFILE ~/.stack_password
+  chmod 600 ~/.stack_password
+  ssh_control_sync_as_user_these_hosts root ~/.stack_password /tmp/.stack_password "$ALL_HOSTS"
+  ssh_control_run_as_user_these_hosts  root "cat /tmp/.stack_password /tmp/.stack_password | passwd stack" "$ALL_HOSTS"
+
+  # Add stack to sudoers on localhost
   cat $SUDO_PASS_FILE | sudo -S ls > /dev/null
-  sudo useradd stack
-  echo st@ck | sudo passwd stack --stdin
   ( sudo grep "stack ALL" /etc/sudoers.d/stack ) || { echo "stack ALL=(root) NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/stack; }
   sudo chmod 0440 /etc/sudoers.d/stack
+  # Add stack to sudoers everywhere
+  sudo cp /etc/sudoers.d/stack /tmp/stack && sudo chown cliff:cliff /tmp/stack
+  ssh_control_sync_as_user_these_hosts root /tmp/stack /etc/sudoers.d/stack "$ALL_HOSTS"
+  ssh_control_run_as_user_these_hosts root "chown root:root /etc/sudoers.d/stack" "$ALL_HOSTS"
+  rm -f /tmp/stack
 }
 
-setup_local_passwordless_sudo () {
-  cat $SUDO_PASS_FILE | sudo -S ls > /dev/null
-  ( sudo grep "cliff ALL" /etc/sudoers.d/cliff ) || { echo "cliff ALL=(root) NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/cliff; }
-  sudo chmod 0440 /etc/sudoers.d/cliff
+setup_stack_keys_and_sync () {
+  [[ -f ~/.password ]] || { 
+    echo "Enter sudo password:" 
+    PASSFILE=`ssh_control_get_password`
+    mv $PASSFILE ~/.password
+    chmod 600 ~/.password
+  }
+  
+  # Set up ~stack/.ssh directory if needed
+  STACK_SSHDIR=~stack/.ssh/
+  cat ~/.password | sudo -S ls >/dev/null
+  sudo su - stack -c '[[ -f $STACK_SSHDIR/id_rsa.pub ]] || ssh-keygen -f $STACK_SSHDIR/id_rsa -P ""'
+  # Put own stack's own pubkey into authorized_keys if needed
+  ADMIN_KEY=`cat $STACK_SSHDIR/pubkeys/id_rsa.pub`
+  KEYPRINT=`echo $ADMIN_KEY | awk '{print $2}'`
+  sudo su - stack -c 'cat $STACK_SSHDIR/id_rsa.pub >> $STACK_SSHDIR/authorized_keys && chmod 600 $STACK_SSHDIR/authorized_keys'
+  sudo su - stack -c "( grep "$KEYPRINT" $STACK_SSHDIR/authorized_keys ) || cat $STACK_SSHDIR/id_rsa.pub >> $STACK_SSHDIR/authorized_keys"
+  # Sync ~stack/.ssh/ to all hosts
+  sudo chown cliff:cliff -R ~stack/
+  ssh_control_sync_as_user_these_hosts root $STACK_SSHDIR/ $STACK_SSHDIR/ "$ALL_HOSTS"
+  ssh_control_run_as_user_these_hosts root "chown stack:stack -R ~stack/" "$ALL_HOSTS"
 }
+
+
+
+
+
+
+
+
+
 
 install_prereqs () {
   cat $SUDO_PASS_FILE | sudo -S ls > /dev/null
@@ -45,14 +96,13 @@ install_prereqs () {
   pip install 'ansible<2.10'
 }
 
-
 install_kolla_for_admin () {
   pip install kolla-ansible
   cat $SUDO_PASS_FILE | sudo -S ls > /dev/null
   sudo mkdir -p /etc/kolla
   sudo chown $USER:$USER /etc/kolla
   cp -r ~/CODE/venvs/kolla-ansible/share/kolla-ansible/etc_examples/kolla/* /etc/kolla
-  cp ~/CODE/venvs/kolla-ansible/share/kolla-ansible/ansible/inventory/* .
+  # cp ~/CODE/venvs/kolla-ansible/share/kolla-ansible/ansible/inventory/* .
 }
 
 install_kolla_for_dev () {
@@ -68,8 +118,8 @@ install_kolla_for_dev () {
 }
 
 config_ansible () {
-  [[ -f /etc/ansible/ansible.cfg ]] || cp ~/CODE/feralcoder/kolla-ansible/files/first-ansible.cfg /etc/ansible/
-  [[ -f ~/ansible.cfg ]] || cp ~/CODE/feralcoder/kolla-ansible/files/first-ansible.cfg ~/
+  [[ -f /etc/ansible/ansible.cfg ]] || cp ~/CODE/feralcoder/kolla-ansible/files/first-ansible.cfg /etc/ansible/ansible.cfg
+  [[ -f ~/ansible.cfg ]] || cp ~/CODE/feralcoder/kolla-ansible/files/first-ansible.cfg ~/ansible.cfg
 }
 
 install_extra_packages () {
@@ -79,13 +129,17 @@ install_extra_packages () {
   sudo dnf config-manager --set-disabled epel-modular epel
 }
 
+
+
 SUDO_PASS_FILE=`get_sudo_password`
+setup_local_passwordless_sudo
+add_stack_user_everywhere
+setup_stack_keys_and_sync
+
 install_prereqs
 install_kolla_for_admin
 #install_kolla_for_dev
 config_ansible
-add_stack_user
-setup_local_passwordless_sudo
 install_extra_packages
 
 rm $SUDO_PASS_FILE
