7c910eedf5b9d7344abbd6d0e79dbdba5bcd0a6d
feralcoder <feralcoder@gmail.com>
2021-02-25 13:57:19 +0000

    Now testing bonds to log file.

--- admin-scripts/macro-management/02a_setup_kolla_and_install.sh
+++ admin-scripts/macro-management/02a_setup_kolla_and_install.sh
@@ -10,13 +10,14 @@ LOG_DIR=~/kolla-ansible-logs/
 ssh_control_run_as_user cliff "mkdir $LOG_DIR" dmb
 ssh_control_run_as_user cliff "$KOLLA_ANSIBLE_CHECKOUT/admin-scripts/setup.sh > $LOG_DIR/01-setup_$NOW.log 2>&1" dmb
 ssh_control_run_as_user cliff "$KOLLA_ANSIBLE_CHECKOUT/admin-scripts/fix-bonds/make_and_setup_stack_bonds.sh > $LOG_DIR/02-bonds_setup_$NOW.log 2>&1" dmb
-ssh_control_run_as_user cliff "$KOLLA_ANSIBLE_CHECKOUT/admin-scripts/validate.sh > $LOG_DIR/03-validate_$NOW.log 2>&1" dmb
+ssh_control_run_as_user cliff "$KOLLA_ANSIBLE_CHECKOUT/admin-scripts/fix-bonds/test_bonds.sh > $LOG_DIR/03-test-bonds_$NOW.log 2>&1" dmb
+ssh_control_run_as_user cliff "$KOLLA_ANSIBLE_CHECKOUT/admin-scripts/validate.sh > $LOG_DIR/04-validate_$NOW.log 2>&1" dmb
 
 os_control_boot_to_target_installation_these_hosts admin "$STACK_HOSTS"
 backup_control_backup_all 02_Kolla-Ansible_Setup
 os_control_boot_to_target_installation_these_hosts default "$STACK_HOSTS"
 
-ssh_control_run_as_user cliff "$KOLLA_ANSIBLE_CHECKOUT/admin-scripts/deploy.sh > $LOG_DIR/03-install_$NOW.log 2>&1" dmb
+ssh_control_run_as_user cliff "$KOLLA_ANSIBLE_CHECKOUT/admin-scripts/deploy.sh > $LOG_DIR/05-install_$NOW.log 2>&1" dmb
 
 os_control_boot_to_target_installation_these_hosts admin "$STACK_HOSTS"
 backup_control_backup_all 03_Kolla-Ansible_Installed
--- admin-scripts/macro-management/02b_restore_kolla_and_install.sh
+++ admin-scripts/macro-management/02b_restore_kolla_and_install.sh
@@ -8,7 +8,7 @@ os_control_boot_to_target_installation_these_hosts admin "$STACK_HOSTS"
 backup_control_restore_all 02_Kolla-Ansible_Setup
 os_control_boot_to_target_installation_these_hosts default "$STACK_HOSTS"
 
-ssh_control_run_as_user cliff "$KOLLA_ANSIBLE_CHECKOUT/admin-scripts/deploy.sh > $LOG_DIR/03-install_$NOW.log 2>&1" dmb
+ssh_control_run_as_user cliff "$KOLLA_ANSIBLE_CHECKOUT/admin-scripts/deploy.sh > $LOG_DIR/05-install_$NOW.log 2>&1" dmb
 
 os_control_boot_to_target_installation_these_hosts admin "$STACK_HOSTS"
 backup_control_backup_all 03_Kolla-Ansible_Installed
