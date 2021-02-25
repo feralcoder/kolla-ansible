8aebad715a3ac3eab3c6fe25f333dab311f619d8
feralcoder <feralcoder@gmail.com>
2021-02-25 12:19:32 +0000

    Setup docker-registry service and use via globals.yml

---
+++ files/docker-registry.sh
@@ -0,0 +1,8 @@
+docker run -d \
+ --name registry \
+ --restart=always \
+ -p 4000:5000 \
+ -v registry:/registry/docker \
+ -e REGISTRY_PROXY_REMOTEURL=https://registry-1.docker.io \
+ registry:2
+
