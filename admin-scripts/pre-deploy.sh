f4e065aad6a66d10b2fab9949a2712ccb05a80f1
feralcoder <feralcoder@gmail.com>
2021-03-02 11:57:00 +0000

    API-net discovery belongs in pre-deploy.

--- admin-scripts/pre-deploy.sh
+++ admin-scripts/pre-deploy.sh
@@ -8,7 +8,12 @@ KOLLA_SETUP_SOURCE="${BASH_SOURCE[0]}"
 KOLLA_SETUP_DIR=$( dirname $KOLLA_SETUP_SOURCE )
 
 
+echo; echo "REFETCHING HOST KEYS FOR API NETWORK EVERYWHERE"
+ssh_control_run_as_user_these_hosts cliff "ssh_control_refetch_hostkey_these_hosts \"$ALL_HOSTS_API_NET\"" "$ALL_HOSTS" 2>/dev/null
+
 kolla-genpwd
 ansible -i $KOLLA_SETUP_DIR/../files/inventory-feralstack all -m ping
 kolla-ansible -i $KOLLA_SETUP_DIR/../files/inventory-feralstack bootstrap-servers
 kolla-ansible -i $KOLLA_SETUP_DIR/../files/inventory-feralstack prechecks
+
+
