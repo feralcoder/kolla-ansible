edc5e829aad999b349c48851533eca7c82c57406
feralcoder <feralcoder@gmail.com>
2021-03-03 00:53:52 +0000

    activate venv

--- admin-scripts/deploy.sh
+++ admin-scripts/deploy.sh
@@ -2,4 +2,6 @@
 KOLLA_SETUP_SOURCE="${BASH_SOURCE[0]}"
 KOLLA_SETUP_DIR=$( dirname $KOLLA_SETUP_SOURCE )
 
+. ~/CODE/venvs/kolla-ansible/bin/activate
+
 kolla-ansible -i ../files/inventory-feralstack deploy
