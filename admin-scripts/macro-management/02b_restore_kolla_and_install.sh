8917b54f4eb36dedc9dab9634efe0abf3c83b380
feralcoder <feralcoder@gmail.com>
2021-03-13 20:06:32 +0000

    Better logging, fix script names, bail out on errors.

--- admin-scripts/macro-management/02b_restore_kolla_and_install.sh
+++ admin-scripts/macro-management/02b_restore_kolla_and_install.sh
@@ -4,11 +4,27 @@ MACRO_DIR=$( dirname $MACRO_SOURCE )
 
 . ~/CODE/feralcoder/host_control/control_scripts.sh
 
+NOW=`date +%Y%m%d-%H%M%S`
+KOLLA_ANSIBLE_CHECKOUT=~/CODE/feralcoder/kolla-ansible/
+LOG_DIR=~/kolla-ansible-logs/
+ANSIBLE_CONTROLLER=dmb
+
+fail_exit () {
+  echo; echo "INSTALLATION FAILED AT STEP: $1"
+  echo "Check the logs and try again.  Or just give up.  I don't care."
+  exit 1
+}
+
 os_control_boot_to_target_installation_these_hosts admin "$STACK_HOSTS"
-backup_control_restore_all 02_Kolla-Ansible_Setup
+backup_control_restore_all 02b_Ceph_Setup
 os_control_boot_to_target_installation_these_hosts default "$STACK_HOSTS"
 
-ssh_control_run_as_user cliff "$KOLLA_ANSIBLE_CHECKOUT/admin-scripts/deploy-kolla.sh > $LOG_DIR/08-deploy-kolla_$NOW.log 2>&1" dmb
+# Run: post-deploy-ceph.sh,  deploy-kolla.sh
+echo; echo "EXECUTING $KOLLA_ANSIBLE_CHECKOUT/admin-scripts/post-deploy-ceph.sh ON $ANSIBLE_CONTROLLER"
+ssh_control_run_as_user cliff "$KOLLA_ANSIBLE_CHECKOUT/admin-scripts/post-deploy-ceph.sh > $LOG_DIR/07-post-deploy-ceph_$NOW.log 2>&1" $ANSIBLE_CONTROLLER || fail_exit "Step 7: Post-Deploy Ceph"
+echo; echo "EXECUTING $KOLLA_ANSIBLE_CHECKOUT/admin-scripts/deploy-kolla.sh ON $ANSIBLE_CONTROLLER"
+ssh_control_run_as_user cliff "$KOLLA_ANSIBLE_CHECKOUT/admin-scripts/deploy-kolla.sh > $LOG_DIR/08-deploy-kolla_$NOW.log 2>&1" $ANSIBLE_CONTROLLER || fail_exit "Step 8: Stack Deployment"
+
 
 os_control_boot_to_target_installation_these_hosts admin "$STACK_HOSTS"
 backup_control_backup_all 03_Kolla-Ansible_Installed
