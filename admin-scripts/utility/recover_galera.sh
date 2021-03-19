b4d3f79f4f16c6ebe70c4eeb204a99f657a6d368
feralcoder <feralcoder@gmail.com>
2021-03-19 16:41:01 +0000

    use kolla-ansible to recover galera

--- admin-scripts/utility/recover_galera.sh
+++ admin-scripts/utility/recover_galera.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-# CHECK GALERA STATE
-docker exec -it  mariadb cat /var/lib/mysql/grastate.dat
-docker stop mariadb
+KOLLA_CHECKOUT=/home/cliff/CODE/feralcoder/kolla-ansible
+KOLLA_VENV=/home/cliff/CODE/venvs/kolla-ansible
+NOW=`date +%Y%m%d-%H%M%S`
+LOG_DIR=~/kolla-ansible-logs/
 
 
-# FIND MASTER NODE / RECOVERY NODE
-# ON HOST: vi /etc/kolla/mariadb/galera.cnf
-# REPLACE: 
-#wsrep_cluster_address = gcomm://172.17.0.210:4567,172.17.0.212:4567,172.17.0.216:4567
-wsrep_cluster_address="gcomm://"
+. $KOLLA_VENV/bin/activate && kolla-ansible -i $KOLLA_CHECKOUT/files/kolla-inventory-feralstack mariadb_recovery >$LOG_DIR/recover_galera_$NOW.log 2>&1
 
-# IN CONTAINER: vi /etc/my.cnf, ADD (start with 1, increase to 6 until success....?)
-innodb_recovery_force=1
+
+
+
+## CHECK GALERA STATE
+#docker exec -it  mariadb cat /var/lib/mysql/grastate.dat
+#docker stop mariadb
+#
+#
+## FIND MASTER NODE / RECOVERY NODE
+## ON HOST: vi /etc/kolla/mariadb/galera.cnf
+## REPLACE: 
+##wsrep_cluster_address = gcomm://172.17.0.210:4567,172.17.0.212:4567,172.17.0.216:4567
+#wsrep_cluster_address="gcomm://"
+#
+## IN CONTAINER: vi /etc/my.cnf, ADD (start with 1, increase to 6 until success....?)
+#innodb_recovery_force=1
 
