6f4ce05ea4b10e4e4d95d8116839cbdb302b96f9
feralcoder <feralcoder@gmail.com>
2021-03-20 07:26:26 +0000

    abundant key fixes required after install.

--- admin-scripts/post-deploy-kolla.sh
+++ admin-scripts/post-deploy-kolla.sh
@@ -37,13 +37,45 @@ place_and_run_init () {
   $XXX
 }
 
-correct_compute_perms () {
-  ssh_control_run_as_user_these_hosts root "docker exec -u root nova_compute mkdir -p /var/run/ceph/guests/ /var/log/qemu/; docker exec -u root nova_compute chown qemu:libvirt /var/run/ceph/guests /var/log/qemu/" "$COMPUTE_HOSTS"
-  ssh_control_run_as_user_these_hosts root "docker exec -u root nova_compute sed -i 's/ceph:x:167:$/ceph:x:167:qemu/g' /etc/group" "$COMPUTE_HOSTS"
+get_libvirt_user_uuid () {
+  local USER=$1
+  HOST=kgn
+  GREPFILE=/tmp/grepuuid_$$
+  echo '#!/bin/bash' > $GREPFILE
+  echo "grep client.$USER /etc/libvirt/secrets/*" >> $GREPFILE && chmod 755 $GREPFILE
+  ssh_control_sync_as_user root $GREPFILE $GREPFILE $HOST >/dev/null 2>&1
+  ssh_control_run_as_user root "docker cp $GREPFILE ${USER}_libvirt:$GREPFILE" $HOST >/dev/null 2>&1
+  UUID=`ssh_control_run_as_user root "docker exec ${USER}_libvirt $GREPFILE" $HOST | grep client.${USER} | awk -F'/' '{print $5}' | awk -F'.' '{print $1}'`
+  echo $UUID
+}
 
+get_user_secret () {
+  local USER=$1
+  KEY=`grep key /etc/ceph/ceph.client.$USER.keyring | awk -F'=' '{print $2}' | sed 's/ //g'`
+  echo $KEY
+}
+
+correct_compute_perms () {
+  # Nova directory perms for ceph
+  ssh_control_run_as_user_these_hosts root "docker exec -u root nova_compute mkdir -p /var/run/ceph/guests/ /var/log/qemu/; docker exec -u root nova_compute chown qemu:libvirt /var/run/ceph/guests /var/log/qemu/" "$COMPUTE_HOSTS"
+  ssh_control_run_as_user_these_hosts root "docker exec -u root nova_compute chmod 775 /var/run/ceph/guests /var/log/qemu/" "$COMPUTE_HOSTS"
+  ssh_control_run_as_user_these_hosts root "docker exec -u root nova_compute usermod -a -G ceph qemu; docker exec -u root nova_compute usermod -a -G ceph libvirt" "$COMPUTE_HOSTS"
+
+  # Nova client key wasn't placed for nova-compute and nova-libvirt
   ssh_control_sync_as_user_these_hosts root /etc/kolla/config/nova/ceph.client.nova.keyring /etc/kolla/nova-compute/ "$COMPUTE_HOSTS"
+  ssh_control_sync_as_user_these_hosts root /etc/kolla/config/nova/ceph.client.nova.keyring /etc/kolla/nova-libvirt/ "$COMPUTE_HOSTS"
+  ssh_control_sync_as_user_these_hosts root /etc/kolla/config/nova/ceph.client.cinder.keyring /etc/kolla/nova-libvirt/ "$COMPUTE_HOSTS"
   ssh_control_run_as_user_these_hosts root "docker cp /etc/kolla/nova-compute/ceph.client.nova.keyring nova_compute:/etc/ceph/ceph.client.nova.keyring" "$COMPUTE_HOSTS"
-  ssh_control_run_as_user_these_hosts root "docker stop nova_compute; docker start nova_compute" "$COMPUTE_HOSTS"
+  ssh_control_run_as_user_these_hosts root "docker cp /etc/kolla/nova-libvirt/ceph.client.nova.keyring nova_libvirt:/etc/ceph/ceph.client.nova.keyring" "$COMPUTE_HOSTS"
+  ssh_control_run_as_user_these_hosts root "docker cp /etc/kolla/nova-libvirt/ceph.client.cinder.keyring nova_libvirt:/etc/ceph/ceph.client.cinder.keyring" "$COMPUTE_HOSTS"
+  ssh_control_run_as_user_these_hosts root "docker stop nova_compute nova_libvirt; docker start nova_compute nova_libvirt" "$COMPUTE_HOSTS"
+
+  # WTF.  Nova user has incorrect key set up in libvirt.
+  UUID_FILE=`get_libvirt_user_uuid nova`.base64
+  echo `get_user_secret nova` > /tmp/$UUID_FILE
+  ssh_control_sync_as_user_these_hosts root /tmp/$UUID_FILE /tmp/$UUID_FILE "$COMPUTE_HOSTS"
+  ssh_control_run_as_user_these_hosts root "docker cp /tmp/$UUID_FILE nova_libvirt:/etc/libvirt/secrets/$UUID_FILE" "$COMPUTE_HOSTS"
+  ssh_control_run_as_user_these_hosts root "rm /tmp/$UUID_FILE" "$COMPUTE_HOSTS"
 }
 
 post_install_install
