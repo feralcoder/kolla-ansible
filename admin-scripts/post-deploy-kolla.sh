a800040b2b84fe3cb9edfc7e7d1345c6eba36c2f
feralcoder <feralcoder@gmail.com>
2021-03-20 03:17:35 +0000

    fix permissions, keys, and folders on nova_compute nodes.  Place custom init-runonce file.

--- admin-scripts/post-deploy-kolla.sh
+++ admin-scripts/post-deploy-kolla.sh
@@ -10,6 +10,7 @@ if [ ! "${BASH_SOURCE[0]}" -ef "$0" ]; then
 fi
 
 . ~/CODE/feralcoder/host_control/control_scripts.sh
+. ~/CODE/venvs/kolla-ansible/bin/activate
 
 ANSIBLE_CONTROLLER=dmb
 
@@ -21,17 +22,29 @@ fail_exit () {
 
 
 
+pip install python-openstackclient
+
+kolla-ansible post-deploy
+. /etc/kolla/admin-openrc.sh
+
+XXX=~/CODE/venvs/kolla-ansible/share/kolla-ansible/init-runonce
+[[ -f $XXX.orig ]] || cp $XXX $XXX.orig
+cp $KOLLA_SETUP_DIR/../files/kolla-init-runonce $XXX
+chmod 755 $XXX
+$XXX
+
+correct_compute_perms () {
+  ssh_control_run_as_user_these_hosts root "docker exec -u root nova_compute mkdir -p /var/run/ceph/guests/ /var/log/qemu/; docker exec -u root nova_compute chown qemu:libvirt /var/run/ceph/guests /var/log/qemu/" "$COMPUTE_HOSTS"
+  ssh_control_run_as_user_these_hosts root "docker exec -u root nova_compute sed -i 's/ceph:x:167:$/ceph:x:167:qemu/g' /etc/group" "$COMPUTE_HOSTS"
+
+  ssh_control_sync_as_user_these_hosts root /etc/kolla/config/nova/ceph.client.nova.keyring /etc/kolla/nova-compute/ "$COMPUTE_HOSTS"
+  ssh_control_run_as_user_these_hosts root "docker cp /etc/kolla/nova-compute/ceph.client.nova.keyring nova_compute:/etc/ceph/ceph.client.nova.keyring" "$COMPUTE_HOSTS"
+  ssh_control_run_as_user_these_hosts root "docker stop nova_compute; docker start nova_compute" "$COMPUTE_HOSTS"
+}
+
 
 # ADD THESE PROPERTIES TO IMAGES IN STACK:
 #hw_scsi_model=virtio-scsi: add the virtio-scsi controller and get better performance and support for discard operation
 #hw_disk_bus=scsi: connect every cinder block devices to that controller
 #hw_qemu_guest_agent=yes: enable the QEMU guest agent
 #os_require_quiesce=yes: send fs-freeze/thaw calls through the QEMU guest agent
-
-
-
-# SET DIRECTORY PERMS ON COMPUTE NODES FOR RBD CACHE AND ADMIN SOCKETS
-# mkdir -p /var/run/ceph/guests/ /var/log/qemu/
-# chown qemu:libvirtd /var/run/ceph/guests /var/log/qemu/
-# (NOTE: check user:group qemu:libvirtd may vary!)
-
