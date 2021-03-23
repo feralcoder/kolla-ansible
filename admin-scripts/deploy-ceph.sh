8a89bb70cc68760c2f1c9bab0a6e8e312e856fe0
feralcoder <feralcoder@gmail.com>
2021-03-23 00:42:01 +0000

    send a page on fail_exit

--- admin-scripts/deploy-ceph.sh
+++ admin-scripts/deploy-ceph.sh
@@ -38,6 +38,7 @@ adjust_firewall () {
 fail_exit () {
   echo; echo "INSTALLATION FAILED AT STEP: $1"
   echo "Check the logs and try again.  Or just give up.  I don't care."
+  python3 ~/CODE/feralcoder/twilio-pager/pager.py "Fallen.  Can't get up.  Installation failed at $1."
   exit 1
 }
 
