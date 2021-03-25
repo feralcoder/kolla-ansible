140094254d0926fa71f59de51d0bfeb944362c89
feralcoder <feralcoder@gmail.com>
2021-03-25 00:11:29 +0000

    don't know how to make docker run use different daemon file; both daemons use same.

--- admin-scripts/deploy-registry.sh
+++ admin-scripts/deploy-registry.sh
@@ -45,8 +45,12 @@ set_up_docker_registry_service () {
   ssh_control_run_as_user root "chown root:root /usr/local/bin/docker-local-registry-start.sh; chmod 755 /usr/local/bin/docker-local-registry-start.sh" $ANSIBLE_CONTROLLER            || return 1
   ssh_control_run_as_user root "chown root:root /usr/local/bin/docker-local-registry-stop.sh; chmod 755 /usr/local/bin/docker-local-registry-stop.sh" $ANSIBLE_CONTROLLER              || return 1
   ssh_control_run_as_user root "chown root:root /etc/systemd/system/docker-local-registry.service; chmod 644 /etc/systemd/system/docker-local-registry.service" $ANSIBLE_CONTROLLER    || return 1
+  # Don't know how to make docker run use other daemon file, next 2 lines are inneffectual
   ssh_control_sync_as_user root $KOLLA_SETUP_DIR/../files/docker-local-daemon.json /etc/docker/local-daemon.json $ANSIBLE_CONTROLLER                                                   || return 1
   ssh_control_run_as_user root "chown root:root /etc/docker/local-daemon.json; chmod 644 /etc/docker/local-daemon.json" $ANSIBLE_CONTROLLER                                            || return 1
+
+  # Don't know how to make docker run use other daemon file, for now both use same...
+  ssh_control_sync_as_user root $KOLLA_SETUP_DIR/../files/docker-local-daemon.json /etc/docker/daemon.json $ANSIBLE_CONTROLLER                                                   || return 1
 
   echo; echo "ENABLING AND STARTING docker-local-registry"
   ssh_control_run_as_user root "systemctl enable docker-local-registry" $ANSIBLE_CONTROLLER   || fail_exit "start docker-local-registry"
@@ -59,6 +63,7 @@ set_up_docker_registry_service () {
   ssh_control_run_as_user root "chown root:root /usr/local/bin/docker-pullthru-registry-start.sh; chmod 755 /usr/local/bin/docker-pullthru-registry-start.sh" $ANSIBLE_CONTROLLER            || return 1
   ssh_control_run_as_user root "chown root:root /usr/local/bin/docker-pullthru-registry-stop.sh; chmod 755 /usr/local/bin/docker-pullthru-registry-stop.sh" $ANSIBLE_CONTROLLER              || return 1
   ssh_control_run_as_user root "chown root:root /etc/systemd/system/docker-pullthru-registry.service; chmod 644 /etc/systemd/system/docker-pullthru-registry.service" $ANSIBLE_CONTROLLER    || return 1
+  # Don't know how to make docker run use other daemon file, next 2 lines are inneffectual
   ssh_control_sync_as_user root $KOLLA_SETUP_DIR/../files/docker-pullthru-daemon.json /etc/docker/pullthru-daemon.json $ANSIBLE_CONTROLLER                                                   || return 1
   ssh_control_run_as_user root "chown root:root /etc/docker/pullthru-daemon.json; chmod 644 /etc/docker/pullthru-daemon.json" $ANSIBLE_CONTROLLER                                            || return 1
 
