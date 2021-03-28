fcdb3b32acdf93a0ad57f4616a6bc1e6569fae08
feralcoder <feralcoder@gmail.com>
2021-03-28 01:49:52 +0000

    tag and push centos images

--- admin-scripts/utility/docker-images/build-images.sh
+++ admin-scripts/utility/docker-images/build-images.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 export YUM_REPO_IP=192.168.127.220
+DOCKER_LOCAL_REGISTRY=192.168.127.220:4001
 
+NOW=`date +%Y%m%d`
 REPOFILE=centos-feralcoder/feralcoder.repo
 cat $REPOFILE.template | sed "s/<<REPOIP>>/$YUM_REPO_IP/g" > $REPOFILE
 
 for IMAGE in  centos-feralcoder  centos-updated-feralcoder; do
   cd $IMAGE  &&  docker build -t $IMAGE .  &&  cd ..
+  docker tag $IMAGE $DOCKER_LOCAL_REGISTRY/feralcoder/$IMAGE:feralcoder_$NOW
+  docker tag $IMAGE $DOCKER_LOCAL_REGISTRY/feralcoder/$IMAGE:latest
+  docker push $DOCKER_LOCAL_REGISTRY/feralcoder/$IMAGE:feralcoder_$NOW
+  docker push $DOCKER_LOCAL_REGISTRY/feralcoder/$IMAGE:latest
 done
