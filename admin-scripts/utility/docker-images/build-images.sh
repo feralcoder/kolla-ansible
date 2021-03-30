bb828025c83cc8cd454d279113c5b822d6d1ae1b
feralcoder <feralcoder@gmail.com>
2021-03-30 00:07:52 +0000

    Build base container image with self-signed root CA

--- admin-scripts/utility/docker-images/build-images.sh
+++ admin-scripts/utility/docker-images/build-images.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
+CONTAINER_BUILD_SOURCE="${BASH_SOURCE[0]}"
+CONTAINER_BUILD_DIR=$( realpath `dirname $CONTAINER_BUILD_SOURCE` )
+
+# BAIL OUT IF USER SOURCES SCRIPT, INSTEAD OF RUNNING IT
+if [ ! "${BASH_SOURCE[0]}" -ef "$0" ]; then
+  echo "Do not source this script (exits will bail you...)."
+  echo "Run it instead"
+  return 1
+fi
+
+. ~/CODE/venvs/kolla-ansible/bin/activate                                               || fail_exit "venv activate"
+. ~/CODE/feralcoder/host_control/control_scripts.sh
+
+
 
 export YUM_REPO_IP=192.168.127.220
 DOCKER_LOCAL_REGISTRY=192.168.127.220:4001
 
 NOW=`date +%Y%m%d`
-REPOFILE=centos-feralcoder/feralcoder.repo
+REPOFILE=$CONTAINER_BUILD_DIR/centos-feralcoder/feralcoder.repo
 cat $REPOFILE.template | sed "s/<<REPOIP>>/$YUM_REPO_IP/g" > $REPOFILE
 
 for IMAGE in  centos-feralcoder  centos-updated-feralcoder; do
-  cd $IMAGE  &&  docker build -t $IMAGE .  &&  cd ..
+  cd $CONTAINER_BUILD_DIR/$IMAGE  &&  docker build -t $IMAGE .  &&  cd ..
   docker tag $IMAGE $DOCKER_LOCAL_REGISTRY/feralcoder/$IMAGE:feralcoder_$NOW
   docker tag $IMAGE $DOCKER_LOCAL_REGISTRY/feralcoder/$IMAGE:8
   docker tag $IMAGE $DOCKER_LOCAL_REGISTRY/feralcoder/$IMAGE:latest
