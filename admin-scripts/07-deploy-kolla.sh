7dfcd7059b8fb3e2e06e26fbe59f8ed647c334e7
feralcoder <feralcoder@gmail.com>
2021-03-30 16:40:02 +0000

    Pull bail-if-sourced logic back into script

--- admin-scripts/07-deploy-kolla.sh
+++ admin-scripts/07-deploy-kolla.sh
@@ -3,9 +3,10 @@ KOLLA_SETUP_SOURCE="${BASH_SOURCE[0]}"
 KOLLA_SETUP_DIR=$( realpath `dirname $KOLLA_SETUP_SOURCE` )
 
 . $KOLLA_SETUP_DIR/common.sh
-bail_if_sourced
-source_host_control_scripts       || fail_exit "source_host_control_scripts"
-use_venv kolla-ansible            || fail_exit "use_venv kolla-ansible"
+[ "${BASH_SOURCE[0]}" -ef "$0" ]  || { echo "Don't source this script!  Run it."; return 1; }
+
+source_host_control_scripts         || fail_exit "source_host_control_scripts"
+use_venv kolla-ansible              || fail_exit "use_venv kolla-ansible"
 
 
 use_localized_containers () {
