84c5a07528a928baeb870ce7127bdb89b28c43e5
feralcoder <feralcoder@gmail.com>
2021-03-31 22:39:54 +0000

    destroy-rebuild now pulls changes and regenerates globals.yml

--- admin-scripts/utility/destroy-and-rebuild-stack.sh
+++ admin-scripts/utility/destroy-and-rebuild-stack.sh
@@ -10,12 +10,38 @@ source_host_control_scripts       || fail_exit "source_host_control_scripts"
 use_venv kolla-ansible            || fail_exit "use_venv kolla-ansible"
 
 
+pull_changes () {
+  cd $UTILITY_DIR
+  NO_CHANGES=`git stash push | grep 'No local changes'`
+  git pull
+  if [[ $NO_CHANGES == "" ]]; then git stash pop; fi
+}
 
-SERVERS=`openstack server list | grep -v '\-\-\-\-\|ID' | awk '{print $2}'`
-for SERVER in $SERVERS; do
-  openstack server delete $SERVER
-done
+regenerate_global_conf () {
+  if ( grep "CONTAINERS FROM FERALCODER"  /etc/kolla/globals.yml ); then
+    cat $UTILITY_DIR/../../files/kolla-globals-localpull.yml $UTILITY_DIR/../../files/kolla-globals-remainder.yml > /etc/kolla/globals.yml
+  elif ( grep "CONTAINERS FROM DOCKER"  /etc/kolla/globals.yml ); then
+    cat $UTILITY_DIR/../../files/kolla-globals-dockerpull.yml $UTILITY_DIR/../../files/kolla-globals-remainder.yml > /etc/kolla/globals.yml
+  fi
+}
 
-kolla-ansible -i $UTILITY_DIR/../../files/kolla-inventory-feralstack destroy     --yes-i-really-really-mean-it && 
-$UTILITY_DIR/../07-deploy-kolla.sh && 
-$UTILITY_DIR/../08-post-deploy-kolla.sh
+destroy_vms () {
+  SERVERS=`openstack server list | grep -v '\-\-\-\-\|ID' | awk '{print $2}'`
+  for SERVER in $SERVERS; do
+    openstack server delete $SERVER
+  done
+}
+
+destroy_and_rebuild () {
+  kolla-ansible -i $UTILITY_DIR/../../files/kolla-inventory-feralstack destroy     --yes-i-really-really-mean-it
+  $UTILITY_DIR/../07-deploy-kolla.sh && 
+  $UTILITY_DIR/../08-post-deploy-kolla.sh
+}
+
+
+
+
+pull_changes            || fail_exit "pull_changes"
+regenerate_global_conf  || fail_exit "regenerate_global_conf"
+destroy_vms             || fail_exit "destroy_vms"
+destroy_and_rebuild     || fail_exit "destroy_and_rebuild"
