c07b9b67e4387ab62914a8cf3d1254365744f64f
feralcoder <feralcoder@gmail.com>
2021-04-05 11:01:27 +0000

    testing

--- admin-scripts/utility/build-containers.sh
+++ admin-scripts/utility/build-containers.sh
@@ -29,11 +29,12 @@ install_packages () {
 
 setup_kolla () {
   mkdir -p ~/CODE/openstack && cd ~/CODE/openstack       || return 1
+  pwd
   git clone https://github.com/openstack/kolla.git       || { cd kolla && git pull; }       || return 1
   cd kolla
+  pwd
   git checkout stable/victoria                           || return 1
-  cd ..
-  pip3 install kolla/                                    || return 1
+  pip3 install ./kolla                                    || return 1
   pip3 install tox                                       || return 1
 }
 
