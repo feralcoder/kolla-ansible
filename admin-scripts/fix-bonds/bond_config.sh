4069c2a04213c123e0e10b2f52470f0069faa9da
feralcoder <feralcoder@gmail.com>
2021-04-07 19:19:39 +0000

    remove bond7 from compute hosts

--- admin-scripts/fix-bonds/bond_config.sh
+++ admin-scripts/fix-bonds/bond_config.sh
@@ -8,6 +8,6 @@ API_SERVER_BONDS="dmb:enp2s0f2:bond3 kgn:eno3:bond3 neo:eno3:bond3 bmn:eno3:bond
 STR_SERVER_BONDS="dmb:enp2s0f3:bond4 kgn:eno4:bond4 neo:eno4:bond4 bmn:eno4:bond4 lmn:eno4:bond4 mtn:eno4:bond4 str:eno4:bond4 mrl:enp2s0f3:bond4 gnd:enp2s0f3:bond4"
 STM_SERVER_BONDS="dmb:enp2s0f4:bond5 kgn:ens5f0:bond5 neo:ens5f0:bond5 bmn:ens5f0:bond5 lmn:ens5f0:bond5 mtn:ens5f0:bond5 str:eno5:bond5 mrl:enp2s0f4:bond5 gnd:enp2s0f4:bond5"
 TNT_SERVER_BONDS="dmb:enp2s0f5:bond6 kgn:ens5f1:bond6 neo:ens5f1:bond6 bmn:ens5f1:bond6 lmn:ens5f1:bond6 mtn:ens5f1:bond6 str:eno6:bond6 mrl:enp2s0f5:bond6 gnd:enp2s0f5:bond6"
-KXT_SERVER_BONDS="dmb:enp2s0f6:bond7 kgn:ens5f1:bond7 neo:ens5f1:bond7 bmn:ens5f1:bond7 lmn:ens5f1:bond7 mtn:ens5f1:bond7 str:eno8:bond7 mrl:enp2s0f6:bond7 gnd:enp2s0f6:bond7"
+KXT_SERVER_BONDS="dmb:enp2s0f6:bond7 str:eno8:bond7 mrl:enp2s0f6:bond7 gnd:enp2s0f6:bond7"
 
 
