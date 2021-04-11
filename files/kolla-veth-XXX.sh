0554205c2949a9a6c6e16f51188e3e55cd81a6c5
feralcoder <feralcoder@gmail.com>
2021-04-11 17:56:07 +0000

    abstracted provder network setup

--- files/kolla-veth-XXX.sh
+++ files/kolla-veth-XXX.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ip link add v-lbaas-vlan type veth peer name v-lbaas
-ip addr add __IP__ dev v-lbaas
-ip link set v-lbaas-vlan up
-ip link set v-lbaas up
+ip link add v-__NETNAME__-vlan type veth peer name v-__NETNAME__
+ip addr add __IP__ dev v-__NETNAME__
+ip link set v-__NETNAME__-vlan up
+ip link set v-__NETNAME__ up
