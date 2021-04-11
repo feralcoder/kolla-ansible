582718b7735ca40f80a54a4dc487cea8d63196b6
feralcoder <feralcoder@gmail.com>
2021-04-11 01:39:14 +0000

    fixup

--- admin-scripts/utility/recover_galera.sh
+++ admin-scripts/utility/recover_galera.sh
@@ -27,7 +27,7 @@ kolla-ansible -i $KOLLA_CHECKOUT/files/kolla-inventory-feralstack mariadb_recove
 ## FIND MASTER NODE / RECOVERY NODE
 ## ON HOST: vi /etc/kolla/mariadb/galera.cnf
 ## REPLACE: 
-##wsrep_cluster_address = gcomm://172.17.0.210:4567,172.17.0.212:4567,172.17.0.216:4567
+##wsrep_cluster_address = gcomm://172.19.4.210:4567,172.19.4.212:4567,172.19.4.216:4567
 #wsrep_cluster_address="gcomm://"
 #
 ## IN CONTAINER: vi /etc/my.cnf, ADD (start with 1, increase to 6 until success....?)
