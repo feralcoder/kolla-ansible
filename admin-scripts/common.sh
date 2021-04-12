aa7052d75292b399535fba0cee83b3ac1641add0
feralcoder <feralcoder@gmail.com>
2021-04-12 01:35:56 +0000

    finish provider net setup

--- admin-scripts/common.sh
+++ admin-scripts/common.sh
@@ -50,16 +50,20 @@ setup_provider_net () {
   local PROVIDER_NETNAME=$1 PROVIDER_VLAN_ID=$2 PROVIDER_SUBNET=$3 PROVIDER_ROUTER_IP=$4 PROVIDER_SUBNET_START=$5 PROVIDER_SUBNET_END=$6
   ssh_control_sync_as_user_these_hosts root ~/CODE/feralcoder/kolla-ansible/files/kolla-veth-XXX.sh /usr/local/bin/veth-$PROVIDER_NETNAME.sh "$CONTROL_HOSTS"  || return 1
   ssh_control_run_as_user_these_hosts root "chmod 744 /usr/local/bin/veth-$PROVIDER_NETNAME.sh" "$CONTROL_HOSTS"  || return 1
-  for HOST in $CONTROL_HOSTS; do
-    LAST_OCTET=`ssh_control_run_as_user root "ip addr" $HOST | grep 192.168.127 | grep inet | awk '{print $2}' | awk -F'/' '{print $1}' | awk -F'.' '{print $4}'`  || return 1
-    ssh_control_run_as_user root "sed -i 's|__IP__|172.30.1.$LAST_OCTET/24|g' /usr/local/bin/veth-$PROVIDER_NETNAME.sh" $HOST  || return 1
-    ssh_control_run_as_user root "sed -i 's|___NETNAME__|$PROVIDER_NETNAME|g' /usr/local/bin/veth-$PROVIDER_NETNAME.sh" $HOST  || return 1
-  done
 
   ssh_control_sync_as_user_these_hosts root ~/CODE/feralcoder/kolla-ansible/files/kolla-veth-XXX.service /etc/systemd/system/veth-$PROVIDER_NETNAME.service "$CONTROL_HOSTS"  || return 1
   ssh_control_run_as_user_these_hosts root "chmod 644 /etc/systemd/system/veth-$PROVIDER_NETNAME.service" "$CONTROL_HOSTS"  || return 1
   
+  for HOST in $CONTROL_HOSTS; do
+    LAST_OCTET=`ssh_control_run_as_user root "ip addr" $HOST | grep 192.168.127 | grep inet | awk '{print $2}' | awk -F'/' '{print $1}' | awk -F'.' '{print $4}'`  || return 1
+    ssh_control_run_as_user root "sed -i 's|__IP__|172.30.1.$LAST_OCTET/24|g' /usr/local/bin/veth-$PROVIDER_NETNAME.sh" $HOST  || return 1
+    ssh_control_run_as_user root "sed -i 's|__NETNAME__|$PROVIDER_NETNAME|g' /usr/local/bin/veth-$PROVIDER_NETNAME.sh" $HOST  || return 1
+    ssh_control_run_as_user root "sed -i 's|__NETNAME__|$PROVIDER_NETNAME|g' /etc/systemd/system/veth-$PROVIDER_NETNAME.service" $HOST  || return 1
+  done
+
   ssh_control_run_as_user_these_hosts root "systemctl daemon-reload" "$CONTROL_HOSTS"  || return 1
+  ssh_control_run_as_user_these_hosts root "systemctl disable veth-$PROVIDER_NETNAME.service" "$CONTROL_HOSTS"  || return 1
+  ssh_control_run_as_user_these_hosts root "systemctl stop veth-$PROVIDER_NETNAME.service" "$CONTROL_HOSTS"  || return 1
   ssh_control_run_as_user_these_hosts root "systemctl enable veth-$PROVIDER_NETNAME.service" "$CONTROL_HOSTS"  || return 1
   ssh_control_run_as_user_these_hosts root "systemctl start veth-$PROVIDER_NETNAME.service" "$CONTROL_HOSTS"  || return 1
   
