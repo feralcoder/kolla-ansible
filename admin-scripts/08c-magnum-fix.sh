02b71baddb3d5704bd2469149f70e3261ebaffa7
feralcoder <feralcoder@gmail.com>
2021-04-21 21:41:50 +0000

    cluster trust needed for cinder volume driver

--- admin-scripts/08c-magnum-fix.sh
+++ admin-scripts/08c-magnum-fix.sh
@@ -15,14 +15,18 @@ SUDO_PASS_FILE=`admin_control_get_sudo_password`    || fail_exit "admin_control_
 
 
 
-fix_magnum_config () {
+fix_magnum_trust_config () {
   ssh_control_run_as_user_these_hosts root "sed -i 's/^cluster_user_trust.*/cluster_user_trust = True/g' /etc/kolla/magnum-conductor/magnum.conf" "$CONTROL_HOSTS"
   ssh_control_run_as_user_these_hosts root "docker container stop magnum_conductor; docker container start magnum_conductor" "$CONTROL_HOSTS"
 }
+#fix_magnum_cloud_provider_config () {
+#  ssh_control_run_as_user_these_hosts root "sed -i 's/^cluster_user_trust.*/cluster_user_trust = True/g' /etc/kolla/magnum-conductor/magnum.conf" "$CONTROL_HOSTS"
+#  ssh_control_run_as_user_these_hosts root "docker container stop magnum_conductor; docker container start magnum_conductor" "$CONTROL_HOSTS"
+#}
 
 
 use_venv kolla-ansible
 cat $SUDO_PASS_FILE | sudo -S ls > /dev/null    || return 1
-fix_magnum_config                           || fail_exit "fix_magnum_config"
+fix_magnum_trust_config                           || fail_exit "fix_magnum_trust_config"
 
 [[ $SUDO_PASS_FILE == ~/.password ]]                || rm $SUDO_PASS_FILE
