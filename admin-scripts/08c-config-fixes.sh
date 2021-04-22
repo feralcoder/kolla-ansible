7af5673f071d5ac96e175d4ef4701364f93f06aa
feralcoder <feralcoder@gmail.com>
2021-04-22 18:22:30 +0000

    capture post-install config editing for general services

--- admin-scripts/08c-config-fixes.sh
+++ admin-scripts/08c-config-fixes.sh
@@ -15,6 +15,10 @@ SUDO_PASS_FILE=`admin_control_get_sudo_password`    || fail_exit "admin_control_
 
 
 
+fix_horizon_timeout_config () {
+  ssh_control_run_as_user_these_hosts root "(grep SESSION_TIMEOUT /etc/kolla/horizon/local_settings) && sed -i 's/^.*SESSION_TIMEOUT.*/SESSION_TIMEOUT = 86400/g' /etc/kolla/horizon/local_settings || echo 'SESSION_TIMEOUT = 86400' >> /etc/kolla/horizon/local_settings" "$CONTROL_HOSTS"
+  ssh_control_run_as_user_these_hosts root "docker container stop horizon; docker container start horizon" "$CONTROL_HOSTS"
+}
 fix_magnum_trust_config () {
   ssh_control_run_as_user_these_hosts root "sed -i 's/^cluster_user_trust.*/cluster_user_trust = True/g' /etc/kolla/magnum-conductor/magnum.conf" "$CONTROL_HOSTS"
   ssh_control_run_as_user_these_hosts root "docker container stop magnum_conductor; docker container start magnum_conductor" "$CONTROL_HOSTS"
@@ -27,6 +31,8 @@ fix_magnum_trust_config () {
 
 use_venv kolla-ansible
 cat $SUDO_PASS_FILE | sudo -S ls > /dev/null    || return 1
+
+fix_horizon_timeout_config                        || fail_exit "fix_horizon_timeout_config"
 fix_magnum_trust_config                           || fail_exit "fix_magnum_trust_config"
 
 [[ $SUDO_PASS_FILE == ~/.password ]]                || rm $SUDO_PASS_FILE
