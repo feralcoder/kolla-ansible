129b3c3361cbbb26ec4a354a3f91ab05b589dbb3
feralcoder <feralcoder@gmail.com>
2021-05-25 02:24:51 +0000

    remove fastly_cache_warmer - replaced

--- admin-scripts/macro-management/02a_setup_kolla_and_install.sh
+++ admin-scripts/macro-management/02a_setup_kolla_and_install.sh
@@ -49,6 +49,7 @@ remediate_hosts () {
   ssh_control_run_as_user_these_hosts cliff "~/CODE/feralcoder/workstation/setup.sh" "$ALL_HOSTS" || return 1
   ssh_control_run_as_user_these_hosts cliff "~/CODE/feralcoder/workstation/update.sh" "$ALL_HOSTS" || return 1
   ssh_control_run_as_user cliff "mkdir -p ~/LOGS/" $ANSIBLE_CONTROLLER || return 1
+  ssh_control_run_as_user root "rm /etc/cron.d/fastly_cache_warmer"  $ANSIBLE_CONTROLLER || return 1
   ssh_control_run_as_user root "echo 'Refresh Fastly Caches Daily at 1AM' > /etc/cron.d/refresh_fastly" $ANSIBLE_CONTROLLER || return 1
   ssh_control_run_as_user root "echo '0 1 * * * cliff /home/cliff/CODE/feralcoder/host_control/scripts/fastly_refresh.sh > /home/cliff/LOGS/fastly_refresh_\`date +\%Y\%m\%d_\%H\%M\`.log 2>&1 ' >> /etc/cron.d/refresh_fastly" $ANSIBLE_CONTROLLER || return 1
 
