5d0b71dde8776be5da13d51acea617d61a5cebbf
feralcoder <feralcoder@gmail.com>
2021-05-27 22:32:22 +0000

    templatize dockerfiles, add stream8 build

--- admin-scripts/utility/docker-images/build-centos-feralcoder-images.sh
+++ admin-scripts/utility/docker-images/build-centos-feralcoder-images.sh
@@ -14,15 +14,21 @@ export YUM_REPO_IP=192.168.127.220
 DOCKER_LOCAL_REGISTRY=192.168.127.220:4001
 
 NOW=`date +%Y%m%d`
-REPOFILE=$CONTAINER_BUILD_DIR/centos-feralcoder/feralcoder.repo
-cat $REPOFILE.template | sed "s/<<REPOIP>>/$YUM_REPO_IP/g" > $REPOFILE
 
-for IMAGE in  centos-feralcoder  centos-updated-feralcoder; do
-  cd $CONTAINER_BUILD_DIR/$IMAGE  &&  docker build -t $IMAGE .  &&  cd ..
-  docker tag $IMAGE $DOCKER_LOCAL_REGISTRY/feralcoder/$IMAGE:feralcoder_$NOW
-  docker tag $IMAGE $DOCKER_LOCAL_REGISTRY/feralcoder/$IMAGE:8
-  docker tag $IMAGE $DOCKER_LOCAL_REGISTRY/feralcoder/$IMAGE:latest
-  docker push $DOCKER_LOCAL_REGISTRY/feralcoder/$IMAGE:feralcoder_$NOW
-  docker push $DOCKER_LOCAL_REGISTRY/feralcoder/$IMAGE:8
-  docker push $DOCKER_LOCAL_REGISTRY/feralcoder/$IMAGE:latest
+for RELEASE in 8 stream8; do
+  UPDATED_DOCKERFILE=$CONTAINER_BUILD_DIR/centos-updated-feralcoder-$RELEASE/Dockerfile
+  cat $UPDATED_DOCKERFILE.template | sed "s/<<REGISTRY>>/$DOCKER_LOCAL_REGISTRY/g" > $UPDATED_DOCKERFILE
+  BASE_REPOFILE=$CONTAINER_BUILD_DIR/centos-feralcoder-$RELEASE/feralcoder.repo
+  cat $BASE_REPOFILE.template | sed "s/<<REPOIP>>/$YUM_REPO_IP/g" > $BASE_REPOFILE
+  for IMAGE in  centos-feralcoder  centos-updated-feralcoder; do
+    echo; echo; echo
+    cd $CONTAINER_BUILD_DIR/$IMAGE-$RELEASE  &&  docker build -t $IMAGE .  &&  cd ..
+    docker tag $IMAGE $DOCKER_LOCAL_REGISTRY/feralcoder/$IMAGE:$RELEASE-$NOW
+    docker tag $IMAGE $DOCKER_LOCAL_REGISTRY/feralcoder/$IMAGE:$RELEASE
+    docker tag $IMAGE $DOCKER_LOCAL_REGISTRY/feralcoder/$IMAGE:$RELEASE-latest
+    docker push $DOCKER_LOCAL_REGISTRY/feralcoder/$IMAGE:$RELEASE-$NOW
+    docker push $DOCKER_LOCAL_REGISTRY/feralcoder/$IMAGE:$RELEASE
+    docker push $DOCKER_LOCAL_REGISTRY/feralcoder/$IMAGE:$RELEASE-latest
+    echo; echo; echo
+  done
 done
